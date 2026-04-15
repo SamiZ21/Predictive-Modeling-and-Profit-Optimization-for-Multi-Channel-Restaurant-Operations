7ed6e5ea0cf9f4e04dee0e2e094dda5be828ddc2
SamiZ21 <roronoazoro78abc@gmail.com>
2026-04-15 22:27:14 +0530

    Update restroapp.py

--- restroapp.py
+++ restroapp.py
@@ -3,8 +3,8 @@ import altair as alt
 import pandas as pd 
 from numpy.random import default_rng as rng, random
 import numpy as np
-from sklearn.linear_model import LinearRegression
 from sklearn.ensemble import RandomForestRegressor, GradientBoostingRegressor
+from sklearn.linear_model import LinearRegression
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import r2_score, mean_absolute_error
 from xgboost import XGBRegressor
