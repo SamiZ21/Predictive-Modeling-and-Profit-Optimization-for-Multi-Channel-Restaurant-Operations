f1c739bcf990b6d724b0e7c3604a0a4dd3dd93be
SamiZ21 <roronoazoro78abc@gmail.com>
2026-04-15 22:07:42 +0530

    Update restroapp.py

--- restroapp.py
+++ restroapp.py
@@ -1,8 +1,3 @@
-
-
-from narwhals import Date
-from sklearn.metrics import r2_score, mean_absolute_error
-from sklearn.linear_model import LinearRegression
 import streamlit as st
 import plotly.express as px
 import altair as alt
@@ -14,6 +9,8 @@ from numpy.random import default_rng as rng, random
 import numpy as np
 import seaborn as sns
 from sklearn.ensemble import RandomForestRegressor, GradientBoostingRegressor
+from sklearn.metrics import r2_score, mean_absolute_error
+from sklearn.linear_model import LinearRegression
 from sklearn.model_selection import train_test_split
 from xgboost import XGBRegressor
 import warnings
